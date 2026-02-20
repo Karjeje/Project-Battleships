7640b8389d4a74cc88c23269c0f086e26cef7ca6
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-02-20 23:26:46 +0200

    Add tests for the Ship class
    Tests should have been written before writing any code in the Ship.js file...

--- src/tests/Ship.spec.js
+++ src/tests/Ship.spec.js
@@ -1,3 +1,30 @@
-test("adds 1 + 2", () => {
-  expect(1 + 2).toBe(3);
+import Ship from "../Ship";
+
+test("ship stores its length", () => {
+  const ship = new Ship(3);
+  expect(ship.length).toBe(3);
+});
+
+test("ship starts with 0 hits", () => {
+  const ship = new Ship(3);
+  expect(ship.hits).toBe(0);
+});
+
+test("hit() increases hit count", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.hits).toBe(1);
+});
+
+test("isSunk() returns false if hits < length", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
+});
+
+test("isSunk() returns true if hits >= length", () => {
+  const ship = new Ship(2);
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
 });
