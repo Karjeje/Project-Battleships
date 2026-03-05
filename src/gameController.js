4f992fe3bf7bfc5507e2a3ede1da76c839e69b4b
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-03-06 00:50:23 +0200

    Add basic gameplay functions

--- src/gameController.js
+++ src/gameController.js
@@ -8,8 +8,30 @@ const gameController = (() => {
 
   let currentPlayer = player1;
 
+  function switchTurn() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+
+  function attack(x, y) {
+    const enemy = currentPlayer === player1 ? player2 : player1;
+
+    enemy.gameboard.receiveAttack(x, y);
+
+    switchTurn();
+  }
+
+  function checkWinner() {
+    if (player1.gameboard.allShipsSunk()) return player2;
+    if (player2.gameboard.allShipsSunk()) return player1;
+
+    return null;
+  }
+
   return {
     player1,
     player2,
+    switchTurn,
+    attack,
+    checkWinner,
   };
 })();
