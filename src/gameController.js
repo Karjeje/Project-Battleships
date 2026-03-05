0f3facbe3374f039a164cfb7fe6fef1209c42c83
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-03-06 01:03:39 +0200

    Add function for computer attack, two ships for each player

--- src/gameController.js
+++ src/gameController.js
@@ -1,5 +1,3 @@
-import Ship from "./Ship";
-import Gameboard from "./Gameboard";
 import Player from "./Player";
 
 const gameController = (() => {
@@ -20,6 +18,13 @@ const gameController = (() => {
     switchTurn();
   }
 
+  function computerMove() {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+
+    attack(x, y);
+  }
+
   function checkWinner() {
     if (player1.gameboard.allShipsSunk()) return player2;
     if (player2.gameboard.allShipsSunk()) return player1;
@@ -27,11 +32,35 @@ const gameController = (() => {
     return null;
   }
 
+  player1.gameboard.placeShip(new Ship(3), [
+    { x: 0, y: 0, hit: false },
+    { x: 1, y: 0, hit: false },
+    { x: 2, y: 0, hit: false },
+  ]);
+  player1.gameboard.placeShip(new Ship(4), [
+    { x: 2, y: 5, hit: false },
+    { x: 3, y: 5, hit: false },
+    { x: 4, y: 5, hit: false },
+    { x: 5, y: 5, hit: false },
+  ]);
+  player2.gameboard.placeShip(new Ship(3), [
+    { x: 2, y: 5, hit: false },
+    { x: 3, y: 5, hit: false },
+    { x: 4, y: 5, hit: false },
+  ]);
+  player2.gameboard.placeShip(new Ship(4), [
+    { x: 2, y: 0, hit: false },
+    { x: 2, y: 1, hit: false },
+    { x: 2, y: 2, hit: false },
+    { x: 2, y: 3, hit: false },
+  ]);
+
   return {
     player1,
     player2,
     switchTurn,
     attack,
     checkWinner,
+    computerMove,
   };
 })();
