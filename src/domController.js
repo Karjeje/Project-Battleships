637943787d63ff1d412385569625cb9be296dd30
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-03-07 17:36:12 +0200

    Create domController module, add renderBoard function

---
+++ src/domController.js
@@ -0,0 +1,21 @@
+import gameController from "./gameController";
+
+const domController = (() => {
+  const playerBoard = document.querySelector("player-board");
+  const enemnyBoard = document.querySelector("enemy-board");
+
+  function renderBoard(board, container, hideShips = false) {
+    container.innerHTML = "";
+
+    for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 10; x++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.dataset.x = x;
+        cell.dataset.y = y;
+
+        container.appendChild(cell);
+      }
+    }
+  }
+})();
